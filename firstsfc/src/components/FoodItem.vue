e2f1f9b7bdbb753d71919aff126638ca582a389c
Sheryn Mae Abril <sgabril@student.apc.edu.ph>
2026-01-16 04:08:42 +0000

    Edited text

--- firstsfc/src/components/FoodItem.vue
+++ firstsfc/src/components/FoodItem.vue
@@ -9,8 +9,8 @@
 export default {
   data() {
     return {
-      name: 'Apples',
-      message: 'I like apples'
+      name: 'Pears and Dalandan',
+      message: 'I like pears and dalandan '
     }
   }
 };
